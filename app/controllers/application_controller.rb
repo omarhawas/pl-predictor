9877cd31b1aa7ec3aef18fbe2f282c304597eda5
Omar Hawas <omarhawas94@gmail.com>
2024-08-10 16:45:14 -0400

    cleanup starting and small refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,30 @@
 class ApplicationController < ActionController::API
-
     before_action :authorize_user
-
+  
     def current_user
-        @current_user
+      @current_user ||= authorize_user
     end
-
+  
     def authorize_user
-        authorization_token = request.headers["Authorization"]
+      return @current_user if @current_user
+  
+      authorization_token = request.headers["Authorization"]
+  
+      Rails.logger.info authorization_token
+  
+      if authorization_token.blank?
+        render status: 401
+        return
+      end
+  
+      @current_user = User.find_by(auth_token: authorization_token)
+  
+      if @current_user
+        @current_user
+      else
+        render status: 401, json: { message: "Unauthorized action. Login first." }
+      end
 
-        Rails.logger.info authorization_token
-
-        if !authorization_token
-            render status: 401
-            return
-        end
-
-        user = User.where(auth_token: authorization_token).first
-
-        puts "in authorize user"
-
-        if user.present?
-            @current_user = user
-        else
-            render status: 401, json: {message: "Unauthorized action. Login first."}
-        end
+      @current_user
     end
-
-end
+  end
---
+++ app/services/football_api.rb
@@ -0,0 +1,14 @@
+class FootballApi
+
+    def self.get_current_season
+
+
+    end
+
+    def self.get_current_season_teams
+
+
+    end
+
+    
+end

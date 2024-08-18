7ea5213f0e52d285fb125991979c51de901d521b
Omar Hawas <omarhawas94@gmail.com>
2024-08-18 17:23:45 -0400

    updated cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,10 +6,13 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "*",
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+    allow do
+      origins 'http://localhost:3001','http://localhost:3000','https://premier-league-predictor-ui.vercel.app/' # Replace with your actual frontend URLs
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        expose: ['Authorization'],
+        max_age: 600
+    end
 end
